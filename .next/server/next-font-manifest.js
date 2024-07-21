b7fdf8f245cde2dab6ac521ce44103bc122352c5
MacBook Pro <mirshod@macbook-pro-3.local>
2024-07-22 00:54:30 +0500

    first commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/mirshod/Desktop/Najot talim 8-oy/4-dars \\\"Next Js\\\"/uyga vazifa/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CMgTDks9JBRbAFdUezGdhOTPYzPHniPSGEaDOrBw2Mo=\"\n}"
